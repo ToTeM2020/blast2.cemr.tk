121c5c0a908de4c08debc6a13660fcf1e0324e9e
ToTeM2020 <69854947+ToTeM2020@users.noreply.github.com>
2020-09-09 23:15:39 +0500

    match delete

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -91,10 +91,13 @@ class SceneMain extends Phaser.Scene {
                 count++;
             }
         }
+        this.newBlockField = this.blockField;
+
+        this.countComboBlocks();
+
         // console.log(this.blockField[0][0].texture.key);
         console.log(this.blockField)
         this.input.on("pointerdown", this.clickBlock, this);
-
     }
 
     update() {
@@ -102,44 +105,58 @@ class SceneMain extends Phaser.Scene {
 
     clickBlock(pointer) {
         // console.log(this.game.getFrame(), 'pd', pointer.x, pointer.y);
+
         var row = Math.floor((pointer.y - this.fieldGrid.config.startY) / this.fieldGrid.ch),
             col = Math.floor((pointer.x - this.fieldGrid.config.startX) / this.fieldGrid.cw);
 
-        this.newBlockField = this.blockField;
-
-        console.log("New",this.newBlockField);
-        console.log("Нажата: ", row, col);
+        // console.log("New", this.newBlockField);
+        // console.log("Нажата: ", row, col);
 
         if (row >= 0 && row < this.fieldGrid.config.rows && col >= 0 && col < this.fieldGrid.config.cols) {
             var color = this.blockField[row][col].texture.key;
 
             this.searchBlock(row, col, color);
+            var nameBlock = "sameBlock";
+            var nameBlock2 = "freeBlock";
+            var count = 0;
+            let tt = this.checkСountBlocks(nameBlock, count);
+            if (tt > 1) {
+                this.blockField = this.newBlockField;
+                this.deleteBlocks(nameBlock);
+                //this.blockField[row][col].destroy();
+                this.fallBlocks(nameBlock, nameBlock2, count);
+            } else {
+            }
+            // console.log("Кол-во ", tt);
+            this.returnBlocks(nameBlock2);
+
 
             // this.blockField[row][col].destroy();
         }
-
         this.add.rectangle(pointer.x, pointer.y, 8, 8, 0xff00ff);
+        this.countComboBlocks();
         console.log(this.blockField);
     }
 
     searchBlock(row, col, color) {
         var nameBlock = "sameBlock";
         var nameBlock2 = "freeBlock";
+
         if (row >= 0 && row < this.fieldGrid.config.rows && col >= 0 && col < this.fieldGrid.config.cols) {
-            if (this.blockField[row][col].name == nameBlock2) {
-                if (this.blockField[row][col].texture.key == color) {
-                    this.blockField[row][col].name = nameBlock;
+            if (this.newBlockField[row][col].name == nameBlock2) {
+                if (this.newBlockField[row][col].texture.key == color) {
+                    this.newBlockField[row][col].name = nameBlock;
 
                     this.searchBlock(row, col - 1, color);
                     this.searchBlock(row, col + 1, color);
                     this.searchBlock(row - 1, col, color);
                     this.searchBlock(row + 1, col, color);
-                    this.blockField[row][col].name = nameBlock;
-                    console.log("Совпала: ", row, col);
-                    this.blockField[row][col].destroy();
+                    // this.newBlockField[row][col].name = nameBlock;
+                    // console.log("Совпала: ", row, col);
                 }
             }
         }
+        // console.log("Кол-во ", count);
     }
 
     searchBlock2(row, col, color) {
@@ -152,7 +169,7 @@ class SceneMain extends Phaser.Scene {
             if (col - 1 >= 0) {
                 if (this.blockField[row][col - 1].texture.key == color) {
                     // this.blockField[row][col - 1].name = nameBlock;
-                    this.searchBlock2(row,col - 1, color);
+                    this.searchBlock2(row, col - 1, color);
                     // console.log("Левый: ", row, col - 1);
                     // console.log(this.blockField[row][col - 1].name);
                 }
@@ -160,25 +177,127 @@ class SceneMain extends Phaser.Scene {
             if (col + 1 < this.fieldGrid.config.cols) {
                 if (this.blockField[row][col + 1].texture.key == color) {
                     // this.blockField[row][col + 1].name = nameBlock;
-                    this.searchBlock2(row,col + 1, color);
+                    this.searchBlock2(row, col + 1, color);
                     // console.log("Правый: ", row, col + 1);
                 }
             }
             if (row - 1 >= 0) {
                 if (this.blockField[row - 1][col].texture.key == color) {
                     // this.blockField[row - 1][col].name = nameBlock;
-                    this.searchBlock2(row - 1,col, color);
+                    this.searchBlock2(row - 1, col, color);
                     // console.log("Верхний: ", row - 1, col);
                 }
             }
             if (row + 1 < this.fieldGrid.config.rows) {
                 if (this.blockField[row + 1][col].texture.key == color) {
                     // this.blockField[row + 1][col].name = nameBlock;
-                    this.searchBlock2(row + 1,col, color);
+                    this.searchBlock2(row + 1, col, color);
                     // console.log("Нижний: ", row + 1, col);
                 }
             }
             console.log("Совпала: ", row, col);
+        }
+    }
+
+    countComboBlocks(){
+        let arrayBlocksField = [];
+        var nameBlock = "sameBlock";
+        var nameBlock2 = "freeBlock";
+
+        for (var i = 0; i < this.fieldGrid.config.rows; i++) {
+            for (var j = 0; j < this.fieldGrid.config.cols; j++) {
+                var newColor = this.newBlockField[i][j].texture.key;
+                this.searchBlock(i, j, newColor);
+
+                let tt = this.comboBlocks(nameBlock, 0);
+                if (tt > 1) {
+                    arrayBlocksField.push(tt);
+                }
+            }
+        }
+        this.returnBlocks(nameBlock2);
+        console.log("Всего совпадений: ", arrayBlocksField.length);
+
+        return arrayBlocksField.length;
+    }
+
+    comboBlocks(nameBlock, count) {
+        for (var i = 0; i < this.fieldGrid.config.rows; i++) {
+            for (var j = 0; j < this.fieldGrid.config.cols; j++) {
+                if (this.newBlockField[i][j].name == nameBlock) {
+                    this.newBlockField[i][j].name = null;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    checkСountBlocks(nameBlock, count) {
+        for (var i = 0; i < this.fieldGrid.config.rows; i++) {
+            for (var j = 0; j < this.fieldGrid.config.cols; j++) {
+                if (this.newBlockField[i][j].name == nameBlock) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+    deleteBlocks(nameBlock) {
+        for (var i = 0; i < this.fieldGrid.config.rows; i++) {
+            for (var j = 0; j < this.fieldGrid.config.cols; j++) {
+                if (this.blockField[i][j].name == nameBlock) {
+                    this.blockField[i][j].destroy();
+                }
+            }
+        }
+    }
+
+    returnBlocks(nameBlock2) {
+        for (var i = 0; i < this.fieldGrid.config.rows; i++) {
+            for (var j = 0; j < this.fieldGrid.config.cols; j++) {
+                this.blockField[i][j].name = nameBlock2;
+            }
+        }
+    }
+
+    fallBlocks(nameBlock, nameBlock2, count) {
+        let bool = false;
+
+        while (bool == false) {
+            bool = true;
+            for (var j = 0; j < this.fieldGrid.config.cols; j++) {
+                var color = Phaser.Math.Between(0, 4);
+                if (this.blockField[0][j].name == nameBlock) {
+                    this.blockField[0][j] = this.add.image(0, j, this.frames[color]).setInteractive();
+                    this.blockField[0][j].displayWidth = this.fieldGrid.cw * 1;
+                    this.blockField[0][j].displayHeight = this.fieldGrid.ch * 1.1;
+                    this.fieldGrid.placeAt(j, 0, this.blockField[0][j]);
+                    this.blockField[0][j].name = nameBlock2;
+                }
+            }
+            for (var i = 1; i < this.fieldGrid.config.rows; i++) {
+                for (var j = 0; j < this.fieldGrid.config.cols; j++) {
+                    if (this.blockField[i][j].name == nameBlock) {
+                        this.blockField[i][j] = this.add.image(0, j, this.newBlockField[i - 1][j].texture.key).setInteractive();
+                        this.blockField[i][j].displayWidth = this.fieldGrid.cw * 1;
+                        this.blockField[i][j].displayHeight = this.fieldGrid.ch * 1.1;
+                        this.blockField[i - 1][j].name = nameBlock;
+                        this.blockField[i - 1][j].destroy();
+                        this.fieldGrid.placeAt(j, i, this.blockField[i][j]);
+                        this.blockField[i][j].name = nameBlock2;
+
+                    }
+                }
+            }
+
+            count = this.checkСountBlocks(nameBlock, 0);
+            // console.log("====>",count);
+            if (count != 0) {
+                bool = false;
+            }
         }
     }
 }
